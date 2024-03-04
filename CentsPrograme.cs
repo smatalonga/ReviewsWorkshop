591beff2291bb5738098b230bf5d5fd9b504dbb5
Santiago Matalonga <Santiago.Matalonga@uws.ac.uk>
2024-03-04 11:41:46 +0000

    Default Soulution

---
+++ CentsPrograme.cs
@@ -0,0 +1,26 @@
+﻿
+namespace CoinChange.Tests
+{
+    internal class CentsPrograme
+    {
+        internal static int WaysToMakeChange(int amount)
+        {
+            
+            int[] coins = { 1, 5, 10, 25 }; // available coin denominations
+            int[] ways = new int[amount + 1]; // array to store number of ways to make change
+
+            ways[0] = 1; // base case
+
+            foreach (int coin in coins)
+            {
+                for (int i = coin; i <= amount; i++)
+                {
+                    ways[i] += ways[i - coin];
+                }
+            }
+
+            return ways[amount];
+            
+        }
+    }
+}
---
+++ CoinsTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CoinChange.Tests
+{
+    [TestClass]
+    public class CentsTest
+    {
+        [DataTestMethod]
+        [DataRow(1, 1, "1 way: 100 pennies")] 
+        [DataRow(15, 6, "6 ways: as mentioned in the problem statement")]
+        public void TestNumberOfWaysToMakeChange(int amount, int expected, string testCaseName)
+        {
+            var actual = CentsPrograme.WaysToMakeChange(amount);
+            Assert.AreEqual(expected, actual, testCaseName);
+        }
+    }
+}
